800d2852ebad36cd863abe3ce611223d146e34d1
Hessuew <Juhani.Juusola@gmail.com>
2019-09-13 19:30:17 +0300

    2. Assigment

---
+++ 2assigment3/src/main/java/com/example/a2assigment3/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.a2assigment3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+    private boolean button2enabled = true;
+    private int image1hidden = 0;
+    private Button button1 = findViewById((R.id.button2));
+    private ImageView image = findViewById(R.id.image1);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void Button1OnClick(View view){
+        button1.setEnabled(button2enabled);
+
+        if (button2enabled = true) {
+            button2enabled= false;
+        } else{
+            button2enabled= true;
+        }
+    }
+    public void Button2OnClick(View view){
+      image.setVisibility(View.INVISIBLE);
+      image1hidden = 4;
+    }
+    public void Button3OnClick(View view){
+        if(image1hidden == 4)
+            image.setVisibility(View.VISIBLE);
+        image1hidden = 0;
+    }
+}
